771a755b3d96119f894d1b4e2bbc9d6835fb9666
Noe8964 <102301944+Noe8964@users.noreply.github.com>
2023-10-16 02:25:14 +0100

    Add files via upload

---
+++ plot/closseness_centrality.py
@@ -0,0 +1,28 @@
+import shelve
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+wot = "ppmi"
+shelf = shelve.open("./shelfs/closseness_centrality_" + wot)
+centrality=shelf["data"] 
+shelf.close()
+
+
+shelf = shelve.open("./shelfs/global")
+thresholds = shelf["thresholds"]
+shelf.close()
+
+
+n = min(len(centrality["control"]), len(centrality["patient"]))
+data_frame_min = {"threshold": thresholds.tolist()*n, "control": [], "patient": []}
+for type in ["control", "patient"]:
+    for i in range(n):
+        data_frame_min[type] += centrality[type][i]
+df_min = pd.DataFrame(data_frame_min)
+
+sns.set_theme(style="darkgrid")
+sns.lineplot(data=df_min, x="threshold", y="control")
+sns.lineplot(data=df_min, x="threshold", y="patient")
+plt.savefig("./images/Centrality/" + wot + "closseness_min" + ".png")
+plt.show()
