56f71f586630ac539a47d9936672d343187e52d8
Francisco Oliveira <franciscodsdo@protonmail.com>
2023-10-17 17:11:05 +0100

    probability of disease

--- generate-shelfs/global.py
+++ generate-shelfs/global.py
@@ -1,10 +1,14 @@
 import shelve
 import numpy as np
 
+
+thresholds = np.linspace(-1, 1, 11)
+
+for i in range(thresholds.size):
+    thresholds[i] = round(thresholds[i], 1)
+    
 shelf = shelve.open("./shelfs/global")
 
-shelf["thresholds"] = np.linspace(-1, 1, 11)
-
-shelf["the_threshold"] = 0.15
+shelf["thresholds"] = thresholds
 
 shelf.close()
--- plot/ratios.py
+++ plot/ratios.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 wot = "ppmi"
+_min = "min"
 
 shelf = shelve.open("./shelfs/ratios_" + wot)
 ratios = shelf["data"]
@@ -13,37 +14,34 @@ shelf = shelve.open("./shelfs/global")
 thresholds = shelf["thresholds"]
 shelf.close()
 
+if _min == "min":
+    n = min(len(ratios["control"]), len(ratios["patient"]))
 
-""" n = min(len(ratios["control"]), len(ratios["patient"]))
-data_frame_min = {"threshold": thresholds.tolist()*n, "control": [], "patient": []}
-for type in ["control", "patient"]:
-    for i in range(n):
-        data_frame_min[type] += ratios[type][i]
-df_min = pd.DataFrame(data_frame_min)
-
-sns.set_theme(style="darkgrid")
-sns.lineplot(data=df_min, x="threshold", y="control")
-sns.lineplot(data=df_min, x="threshold", y="patient")
-plt.savefig("./images/ratios/" + wot + "_min" + ".png")
-plt.show() """
-
-
-data_frame_creator = {"control": {"threshold": thresholds.tolist()*len(ratios["control"]), "data": []},
-                      "patient": {"threshold": thresholds.tolist()*len(ratios["patient"]), "data": []}} 
-
-for type in ["control", "patient"]:
-    for i in range(len(ratios[type])):
-        data_frame_creator[type]["data"] += ratios[type][i]
+    data_frame_creator = {"control": {"threshold": thresholds.tolist()*n, "data": []},
+                          "patient": {"threshold": thresholds.tolist()*n, "data": []}}
     
-data_frame = {"control": pd.DataFrame(data_frame_creator["control"]),
-              "patient": pd.DataFrame(data_frame_creator["patient"])}
+    for type in ["control", "patient"]:
+        for i in range(n):
+            data_frame_creator[type]["data"] += ratios[type][i]
+else:
+    data_frame_creator = {"control": {"threshold": thresholds.tolist()*len(ratios["control"]), "data": []},
+                          "patient": {"threshold": thresholds.tolist()*len(ratios["patient"]), "data": []}}
+    
+    for type in ["control", "patient"]:
+        for i in range(len(ratios[type])):
+            data_frame_creator[type]["data"] += ratios[type][i]
+
+
+df = {"control": pd.DataFrame(data_frame_creator["control"]),
+      "patient": pd.DataFrame(data_frame_creator["patient"])}
 
 sns.set_theme(style="darkgrid")
-sns.lineplot(data=data_frame["control"], x="threshold", y="data")
-sns.lineplot(data=data_frame["patient"], x="threshold", y="data")
-plt.savefig("./images/ratios/" + wot + "_nmin" + ".png")
+sns.lineplot(data=df["control"], x="threshold", y="data")
+sns.lineplot(data=df["patient"], x="threshold", y="data")
+plt.savefig("./images/ratios/" + wot + "_min" + ".png")
 plt.show()
 
-shelf = shelve.open("./shelfs/data_frame_ratios_" + wot)
-shelf["data"] = data_frame
+shelf = shelve.open("./shelfs/data_frame_ratios_" + _min + "_" + wot)
+shelf["data"] = df
 shelf.close()
+
